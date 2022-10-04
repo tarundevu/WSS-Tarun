c7acb061e23a8d0268f514ee8ac71175e3448d86
tarundevu <tarun.devu@gmail.com>
2022-10-04 14:43:11 +0800

    updated 4th oct

--- src/main/java/frc/robot/commands/TeleCmd.java
+++ src/main/java/frc/robot/commands/TeleCmd.java
@@ -63,7 +63,8 @@ public class TeleCmd extends CommandBase
         s1 = speed_multiplier * ((1*x) + (0*y)+(1*w));
         s2 = speed_multiplier * ((-0.5*x) + (0.865*y)+(1*w));
         m_omnidrive.setMotorOut012(s0,s1,s2);
-        m_arm.setServoAngle((s*150)+150);
+        //m_arm.setServoAngle((s*150)+150);
+        m_arm.setArmPos(0.25, 0);
         //m_omnidrive.setRobotSpeedXYW(x*0.6, y*0.6, w*Math.PI);
 
     }
--- src/main/java/frc/robot/subsystems/Arm.java
+++ src/main/java/frc/robot/subsystems/Arm.java
@@ -1,8 +1,11 @@
 package frc.robot.subsystems;
 
+import java.util.Map;
+
 import com.studica.frc.Servo;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.wpilibj.shuffleboard.BuiltInWidgets;
 import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
 import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
 import edu.wpi.first.networktables.NetworkTableEntry;
@@ -12,13 +15,24 @@ public class Arm extends SubsystemBase{
     private final Servo servo1;
     private final Servo servo2;
     private double Servovalue;
+    private double Servovalue2;
+
+    private final double a1 = 0.25; 
+    private final double a2 = 0.25; 
+    private double offset0 = 0;   //For making software adjustment to servo 
+    private double offset1 = 0;
+    
 
     private final ShuffleboardTab tab = Shuffleboard.getTab("Arm");
     private final NetworkTableEntry D_armvalue = tab.add("Arm value", 0).getEntry();
+    private final NetworkTableEntry D_armvalue2 = tab.add("Arm value2", 0).getEntry();
+    private final NetworkTableEntry D_offset0 = tab.addPersistent("offset0", 0).withWidget(BuiltInWidgets.kNumberSlider).withProperties(Map.of("min", -10, "max", +10)).getEntry();
+    private final NetworkTableEntry D_offset1 = tab.addPersistent("offset1", 0).withWidget(BuiltInWidgets.kNumberSlider).withProperties(Map.of("min", -10, "max", +10)).getEntry();
 
     public Arm(){
-      servo1 = new Servo(8);
+      servo1 = new Servo(0);
       servo2 = new Servo(1);
+      
     }
     
     /**
@@ -31,19 +45,57 @@ public class Arm extends SubsystemBase{
       Servovalue = degrees;
       servo1.setAngle(Servovalue);
     }
+    public void setServoAngle2(final double degrees){
+      Servovalue2 = degrees;
+      servo2.setAngle(Servovalue2);
+    }
 
     public double getServoAngle(){
       return servo1.getAngle();
       
     }
+    public double getServoAngle2(){
+      return servo2.getAngle();
+      
+    }
+
+    public void setArmPos(double _x, double _y){
+    
+      double x = _x;
+      double y = _y;
+
+      double a = a2;
+      double c = a1;
+      double b = Math.sqrt(x*x+y*y);
+      double alpha = Math.acos( (b*b + c*c - a*a)/(2*b*c) );
+      double beta = Math.acos( (a*a + c*c - b*b)/(2*a*c) );
+
+        // A is servo0 angle wrt horizon
+        // When A is zero, arm-c is horizontal.
+        // beta is servo1 angle wrt arm-c (BA)
+        // When beta is zero, arm-c is closed  to arm-c
+      double B = beta;    //Use B to designate beta. Different from diagram.
+      double A = alpha + Math.atan2(y,x);
+
+        //servo0 and servo1 might be mounted clockwise or anti clockwise.
+        //offset0 and offset1 are used to adjust the zero the arm position.
+        //This makes it easier to mount and tune the arm.
+      servo1.setAngle(Math.toDegrees(A) + offset0);
+      servo2.setAngle(Math.toDegrees(B) + offset1);
+    }
+
+        
+
     
     /**
      * code that runs once every robot loop
      */
     @Override
     public void periodic(){
+      offset0 = D_offset0.getDouble(0.0);
+      offset1 = D_offset1.getDouble(0.0);
       D_armvalue.setDouble(getServoAngle());
-
+      D_armvalue2.setDouble(getServoAngle2());
     }
 
     
